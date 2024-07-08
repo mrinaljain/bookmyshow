11a7cfad34ca3483f1fb0c19483ca58d17c6c096
mrinaljain <jain.mrinal140@gmail.com>
2024-07-09 05:13:05 +0530

    sds

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -3,12 +3,11 @@ import mongoose from "mongoose";
 const dbConnect = async () => {
   try {
     const connection = await mongoose.connect(
-      `mongodb+srv://jainmrinal140:${process.env.DATABASE_PASSWORD}@cluster0.5yymuvg.mongodb.net/bookmyshow?retryWrites=true&w=majority&appName=Cluster0`,
-      { useNewUrlParser: true }
+      `mongodb+srv://jainmrinal140:${process.env.DATABASE_PASSWORD}@cluster0.5yymuvg.mongodb.net/bookmyshow?retryWrites=true&w=majority&appName=Cluster0`
     );
 
     if (connection) {
-      console.table(`Welcome to connected Database : ${connection}`);
+      console.table(connection);
     }
   } catch (error) {
     console.log(error.message);
