39d00fbee90b5dd002683edb2722b2d5a4477faf
mrinaljain <jain.mrinal140@gmail.com>
2024-07-28 20:22:38 +0530

    deployment status added localhost removed

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/8027a339-e68b-4e92-849c-1bd158b99250/deploy-status)](https://app.netlify.com/sites/mrinal-bookmyshow/deploys)
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,17 +1,18 @@
-const BASE_URL = "http://localhost";
-const PORT = ":3000";
+// const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://bms-api-dmgc.onrender.com";
+// const PORT = "";
 
-export const LOGIN_API = `${BASE_URL}${PORT}/api/user/login`;
-export const SIGNUP_API = `${BASE_URL}${PORT}/api/user/register`;
-export const USER_PROFILE = `${BASE_URL}${PORT}/api/user/profile`;
-export const LOGOUT = `${BASE_URL}${PORT}/api/user/logout`;
-export const VERIFY_USER = `${BASE_URL}${PORT}/api/user/verifyUser`;
-export const ADD_MOVIE = `${BASE_URL}${PORT}/api/movie/`;
-export const MOVIES_LIST = `${BASE_URL}${PORT}/api/movie/list`;
-export const MOVIE_DETAIL = `${BASE_URL}${PORT}/api/movie/`;
-export const DELETE_MOVIE = `${BASE_URL}${PORT}/api/movie/`;
-export const SHOWS_LIST = `${BASE_URL}${PORT}/api/show/list`;
-export const SHOW_DETAIL = `${BASE_URL}${PORT}/api/show/`;
+export const LOGIN_API = `${BASE_URL}/api/user/login`;
+export const SIGNUP_API = `${BASE_URL}/api/user/register`;
+export const USER_PROFILE = `${BASE_URL}/api/user/profile`;
+export const LOGOUT = `${BASE_URL}/api/user/logout`;
+export const VERIFY_USER = `${BASE_URL}/api/user/verifyUser`;
+export const ADD_MOVIE = `${BASE_URL}/api/movie/`;
+export const MOVIES_LIST = `${BASE_URL}/api/movie/list`;
+export const MOVIE_DETAIL = `${BASE_URL}/api/movie/`;
+export const DELETE_MOVIE = `${BASE_URL}/api/movie/`;
+export const SHOWS_LIST = `${BASE_URL}/api/show/list`;
+export const SHOW_DETAIL = `${BASE_URL}/api/show/`;
 
 export const OPTIONS = {
   method: "GET",
